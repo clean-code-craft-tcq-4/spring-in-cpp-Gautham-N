5ab52aa9a5f9e02914de1062a7ccc21dd1ffb7bd
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-05-16 15:52:13 +0530

    Update stats.h
    Checking all three test case version1.

--- stats.h
+++ stats.h
@@ -1,8 +1,64 @@
 #include <vector>
+#include <iostream>
+class Stats
+{
+public:
+    float average;
+    float min;
+    float max;
+};
+class IAlerter
+{
+public:
+    virtual void updateState()=0;
+};
 
-namespace Statistics {
-    class Stats
+class EmailAlert: public IAlerter
+{
+    
+    void updateState() override
     {
-    };
-    Stats ComputeStatistics(const std::vector<float>& );
+        emailSent = true;
+        std::cout << "EmailAlered called..." << std::endl;
+    }
+public:
+    bool emailSent = false;
+    EmailAlert()
+    {
+        emailSent = false;
+    }
+};
+class LEDAlert: public IAlerter
+{
+
+    void updateState() override 
+    {
+        ledGlows = true;
+        std::cout << "LedAlered called..." << std::endl;
+    }
+public:
+    bool ledGlows;
+    LEDAlert()
+    {
+        ledGlows = false;
+    }
+};
+
+class StatsAlerter
+{
+    int maxThres;
+    std::vector<IAlerter*> IAlerts;
+public:
+    StatsAlerter(int maxThreshold, std::vector<IAlerter*> IAlerters)
+    {
+        maxThres = maxThreshold;
+        IAlerts = IAlerters;
+    }
+    void checkAndAlert(const std::vector<float>& data);
+};
+namespace Statistics {   
+    Stats ComputeStatistics(const std::vector<float>& data);
 }
+
+
+
